6d317d0a50c1f12dec0e9430dfd45dcb7f532684
Imre Gera <hanziness@windowslive.com>
2020-08-09 13:26:34 +0200

    Show approximate timer, use timer preset from store

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <section>
     <schedule-display />
     <div class="text-center">
+      <timer-approximate />
       <promodoro-timer />
     </div>
   </section>
@@ -10,13 +11,15 @@
 <script>
 import PromodoroTimer from '@/components/promodoroTimer.vue'
 import ScheduleDisplay from '@/components/scheduleDisplay.vue'
+import TimerApproximate from '@/components/timers/approximate.vue'
 
 export default {
   components: {
-    PromodoroTimer, ScheduleDisplay
+    PromodoroTimer, ScheduleDisplay, TimerApproximate
   },
 
   mounted () {
+    this.$store.commit('settings/applyPreset', 'debug')
     this.$store.dispatch('events/checkSchedule')
     this.$store.dispatch('timer/setNewTimer', this.$store.state.events.schedule[0]._length)
   }
