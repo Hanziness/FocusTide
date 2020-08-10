a34f8b19cd23e664dc94c6f5ed7cc0446705a104
Imre Gera <hanziness@windowslive.com>
2020-08-10 19:49:49 +0200

    Remove superfluous approximate timer from index

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <section>
     <schedule-display />
     <div class="text-center">
-      <timer-approximate />
       <promodoro-timer />
     </div>
   </section>
@@ -11,11 +10,10 @@
 <script>
 import PromodoroTimer from '@/components/promodoroTimer.vue'
 import ScheduleDisplay from '@/components/scheduleDisplay.vue'
-import TimerApproximate from '@/components/timers/approximate.vue'
 
 export default {
   components: {
-    PromodoroTimer, ScheduleDisplay, TimerApproximate
+    PromodoroTimer, ScheduleDisplay
   },
 
   mounted () {
