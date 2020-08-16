465c1fb6f6e62a81fafafea333f4b68518d2962c
Imre Gera <hanziness@windowslive.com>
2020-08-16 16:07:32 +0200

    Dynamic page icon that reflects timer state

--- pages/index.vue
+++ pages/index.vue
@@ -42,7 +42,22 @@ export default {
   head () {
     return {
       titleTemplate: `(${this.remainingTimeString}) %s`,
-      title: `${this.pageTitle}`
+      title: `${this.pageTitle}`,
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: `data:image/svg+xml,
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  style="color: ${this.$store.getters['events/currentScheduleColour']};"
+                  xmlns="http://www.w3.org/2000/svg"
+                ><circle cx="16" cy="16" r="15.0049" fill="currentColor" /></svg>`
+        }
+      ]
     }
   }
 }
