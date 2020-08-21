b224764fe4f2187fae900d4b7d657c873803664c
Imre Gera <hanziness@windowslive.com>
2020-08-21 14:12:43 +0200

    Settings are initialized earlier

--- pages/index.vue
+++ pages/index.vue
@@ -49,11 +49,15 @@ export default {
     }
   },
 
-  mounted () {
+  beforeCreate () {
     this.$store.commit('settings/applyPreset', 'debug')
     // this.$store.dispatch('events/checkSchedule')
     this.$store.dispatch('timer/setNewTimer', this.$store.getters['events/getSchedule'][0]._length)
     this.$store.dispatch('timer/initDefaultSubscribeFunctions')
+  },
+
+  mounted () {
+
   },
 
   head () {
