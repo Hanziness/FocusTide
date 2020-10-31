77c09865ed6ecb560cfd4aef144dbb30dd29e9ff
Imre Gera <hanziness@windowslive.com>
2020-10-31 11:06:48 +0100

    Fix class and style bindings in functional button component

--- components/tailwinded/base/button.vue
+++ components/tailwinded/base/button.vue
@@ -2,10 +2,11 @@
   <Component
     :is="props.tag"
     :ref="data.ref"
-    :class="['rounded p-4 bg-teal-500 ui-button', data.class, data.staticStyle, {
+    :class="['ui-button', { 'subtle': props.subtle, 'regular': !props.subtle }, data.class, data.staticClass, {
       'cursor-pointer': !props.disabled,
       'disabled': props.disabled
     }]"
+    :style="[data.style, data.staticStyle]"
     v-bind="data.attrs"
     :aria-disabled="props.disabled"
     :disabled="props.disabled"
@@ -16,18 +17,32 @@
 </template>
 
 <style lang="scss" scoped>
-.ui-button {
-  @apply bg-gray-400;
-
+.ui.button {
   transition: background-color 0.1s ease-out;
 }
 
-.ui-button:hover:not(:disabled) {
+.ui-button.regular {
+  @apply bg-gray-400 rounded p-4;
+}
+
+.ui-button.regular:hover:not(:disabled) {
   @apply bg-gray-500;
 }
 
-.ui-button:active:not(:disabled) {
+.ui-button.regular:active:not(:disabled) {
   @apply bg-gray-600;
+}
+
+.ui-button.subtle {
+  @apply bg-transparent rounded-full p-2;
+}
+
+.ui-button.subtle:hover {
+  @apply bg-gray-200;
+}
+
+.ui-button.subtle:active {
+  @apply bg-gray-400;
 }
 
 .ui-button.disabled {
@@ -46,6 +61,10 @@ export default {
       type: String
     },
     disabled: {
+      default: false,
+      type: Boolean
+    },
+    subtle: {
       default: false,
       type: Boolean
     }
