98803b48fb763744665277e372a81582c3e8b754
Imre Gera <hanziness@windowslive.com>
2020-11-17 21:58:43 +0100

    Remove Vuetify related configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,6 @@ export default {
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
-    // '@nuxtjs/vuetify',
     '@nuxtjs/tailwindcss'
   ],
 
@@ -123,28 +122,6 @@ export default {
     // download: true
   },
 
-  /*
-  ** vuetify module configuration
-  ** https://github.com/nuxt-community/vuetify-module
-  */
-  // vuetify: {
-  //   customVariables: ['~/assets/variables.scss'],
-  //   treeShake: true,
-  //   theme: {
-  //     dark: false,
-  //     themes: {
-  //       dark: {
-  //         primary: colors.blue.darken2,
-  //         accent: colors.grey.darken3,
-  //         secondary: colors.amber.darken3,
-  //         info: colors.teal.lighten1,
-  //         warning: colors.amber.base,
-  //         error: colors.deepOrange.accent4,
-  //         success: colors.green.accent3
-  //       }
-  //     }
-  //   }
-  // },
   /*
   ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
