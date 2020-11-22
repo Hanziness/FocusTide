a8b73352b1ad2ccee7b42c4f3a6573f89dbf8789
Imre Gera <hanziness@windowslive.com>
2020-11-22 18:26:11 +0100

    Set `lang` setting to autodetected value

--- plugins/i18nwatcher.client.js
+++ plugins/i18nwatcher.client.js
@@ -1,18 +1,22 @@
 export default function ({ store, app }) {
   if (app.i18n) {
-    const storeLang = store.state.settings.lang
+    const appLang = store.state.settings.lang ? store.state.settings.lang : app.i18n.locale
 
-    if (storeLang) {
+    if (store.state.settings.lang) {
       // set nuxt-i18n language
-      app.i18n.setLocale(storeLang)
-      if (app.$dayjs) {
-        // also set dayJS locale
-        app.$dayjs.dayjs.locale(storeLang)
+      app.i18n.setLocale(appLang)
+    } else {
+      store.commit('settings/SET', { key: ['lang'], value: appLang })
+      app.$dayjs.dayjs.locale(appLang)
+    }
 
-        // and automatically change it when the nuxt-i18n locale changes
-        app.i18n.onLanguageSwitched = (oldLocale, newLocale) => {
-          app.$dayjs.dayjs.locale(newLocale)
-        }
+    if (app.$dayjs) {
+      // also set dayJS locale
+      app.$dayjs.dayjs.locale(appLang)
+
+      // and automatically change it when the nuxt-i18n locale changes
+      app.i18n.onLanguageSwitched = (oldLocale, newLocale) => {
+        app.$dayjs.dayjs.locale(newLocale)
       }
     }
 
