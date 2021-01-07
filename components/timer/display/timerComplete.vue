8dfcf991e1162c30b595eb7348d3525d93af32d0
Imre Gera <hanziness@windowslive.com>
2021-01-07 22:01:28 +0100

    feat: Timer is replaced with a checkmark when completed

---
+++ components/timer/display/timerComplete.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="timer-complete-icon">
+    <complete-icon size="20vw" />
+  </div>
+</template>
+
+<script>
+import CompleteIcon from 'vue-material-design-icons/Check.vue'
+
+export default {
+  components: { CompleteIcon }
+}
+</script>
