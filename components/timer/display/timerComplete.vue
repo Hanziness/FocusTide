ff38a19c22b646a5c75918f25698430d04995e29
Imre Gera <hanziness@windowslive.com>
2021-04-08 14:19:55 +0200

    fix: Fix invalid units warning for checkmark

--- components/timer/display/timerComplete.vue
+++ components/timer/display/timerComplete.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="timer-complete-icon">
-    <complete-icon size="20vw" />
+    <complete-icon />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.timer-complete-icon svg {
+  width: 20vw !important;
+  height: 20vw !important;
+}
+</style>
 
 <script>
 import CompleteIcon from 'vue-material-design-icons/Check.vue'
