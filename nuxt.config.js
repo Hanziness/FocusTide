9fad494b2ac41e8f7aad7cdfb5c36c2678b924b2
Imre Gera <hanziness@windowslive.com>
2021-07-10 21:01:02 +0200

    fix: Language should now be permanent across pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -143,11 +143,11 @@ export default {
       // If enabled, a cookie is set once a user has been redirected to his
       // preferred language to prevent subsequent redirections
       // Set to false to redirect every time
-      useCookie: false,
+      useCookie: true,
       // Set to override the default domain of the cookie. Defaults to host of the site.
       cookieDomain: null,
       // Cookie name
-      cookieKey: 'i18n_redirected',
+      cookieKey: 'lang',
       // Set to always redirect to value stored in the cookie, not just once
       alwaysRedirect: false,
       // If no locale for the browsers locale is a match, use this one as a fallback
