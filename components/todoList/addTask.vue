45d3ffeb9d94a737a589cfbf695ce294ee670ea4
Imre Gera <hanziness@windowslive.com>
2021-12-24 18:50:34 +0100

    feat(tasks): Add new task with enter

--- components/todoList/addTask.vue
+++ components/todoList/addTask.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-row space-x-2 rounded-xl bg-gray-100 py-4 pl-4 pr-2 focus-within:bg-white focus-within:shadow-lg shadow-sm transition-all duration-500 focus-within:duration-200 items-center" :style="{ '--theme': $store.getters['schedule/currentScheduleColour'] }">
-    <input v-model="taskTitle" type="text" required class="block min-w-0 bg-transparent border-none rounded-full focus:ring-transparent peer flex-grow p-0">
+    <input v-model="taskTitle" type="text" required class="block min-w-0 bg-transparent border-none rounded-full focus:ring-transparent peer flex-grow p-0" @keyup="checkEnter">
     <button class="peer-invalid:bg-gray-200 peer-invalid:opacity-60 peer-invalid:pointer-events-none rounded-lg bg-theme bg-opacity-80 hover:bg-opacity-100 text-white px-3 py-2 -my-3 transition-all" @click="addTask">
       <IconReturn />
     </button>
@@ -24,6 +24,13 @@ export default {
     addTask () {
       this.$store.commit('tasklist/newTask', { title: this.taskTitle, section: this.$store.getters['schedule/getCurrentItem'].type, state: this.taskState })
       this.taskTitle = ''
+    },
+
+    // Add new task when enter is pressed
+    checkEnter (event) {
+      if (event.code.toLowerCase() === 'enter') {
+        this.addTask()
+      }
     }
   }
 }
