85d71532474734cc530b0a844be65535e74375bc
Imre Gera <hanziness@windowslive.com>
2021-12-24 19:41:50 +0100

    feat(tasks): Validate input task name

--- components/todoList/addTask.vue
+++ components/todoList/addTask.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row space-x-2 rounded-xl bg-gray-100 py-4 pl-4 pr-2 focus-within:bg-white focus-within:shadow-lg shadow-sm transition-all duration-500 focus-within:duration-200 items-center" :style="{ '--theme': $store.getters['schedule/currentScheduleColour'] }">
     <input v-model="taskTitle" type="text" required class="block min-w-0 bg-transparent border-none rounded-full focus:ring-transparent peer flex-grow p-0" @keyup="checkEnter">
-    <button class="peer-invalid:bg-gray-200 peer-invalid:opacity-60 peer-invalid:pointer-events-none rounded-lg bg-theme bg-opacity-80 hover:bg-opacity-100 text-white px-3 py-2 -my-3 transition-all" @click="addTask">
+    <button :class="['rounded-lg bg-theme bg-opacity-80 hover:bg-opacity-100 text-white px-3 py-2 -my-3 transition-all', { 'bg-gray-200 opacity-60 pointer-events-none': !valid }]" @click="addTask">
       <IconReturn />
     </button>
   </div>
@@ -13,13 +13,30 @@ import { taskState } from '@/store/tasklist.js'
 
 export default {
   components: { IconReturn },
+
   data () {
     return {
       taskTitle: '',
-      taskSection: 'work',
-      taskState: taskState.inProgress
+      taskState: taskState.inProgress,
+      valid: false
     }
   },
+
+  watch: {
+    taskTitle (newValue) {
+      if (newValue.length < 1) {
+        this.valid = false
+        return
+      }
+
+      const matchingTasks = this.$store.state.tasklist.tasks.filter(
+        task => task.title === newValue && task.section === this.$store.getters['schedule/getCurrentItem'].type
+      )
+
+      this.valid = matchingTasks.length === 0
+    }
+  },
+
   methods: {
     addTask () {
       this.$store.commit('tasklist/newTask', { title: this.taskTitle, section: this.$store.getters['schedule/getCurrentItem'].type, state: this.taskState })
@@ -28,7 +45,7 @@ export default {
 
     // Add new task when enter is pressed
     checkEnter (event) {
-      if (event.code.toLowerCase() === 'enter') {
+      if (this.valid && event.code.toLowerCase() === 'enter') {
         this.addTask()
       }
     }
