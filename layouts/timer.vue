9310c3a82c99454c40a9c9a83a9e7f93ba6007e5
Imre Gera <hanziness@windowslive.com>
2021-12-31 20:49:31 +0100

    feat: Add page transitions (#129)

--- layouts/timer.vue
+++ layouts/timer.vue
@@ -1,21 +1,26 @@
 <template>
-  <div :class="['w-screen, h-screen relative', { 'dark': darkMode }]">
-    <nuxt />
-    <!-- <portal to="footer">
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </portal> -->
-  </div>
+  <transition name="loading" mode="in-out">
+    <LoadingIndicator v-if="loading" />
+    <div v-if="!loading" :class="['w-screen, h-screen relative', { 'dark': darkMode }]">
+      <nuxt />
+    </div>
+  </transition>
 </template>
 
 <script>
+import LoadingIndicator from '@/components/timer/loading.vue'
+
 export default {
   name: 'LayoutTimer',
   components: {
+    LoadingIndicator
   },
   data () {
     return {
+      loading: true
     }
   },
+
   computed: {
     darkMode () {
       return this.$store.state.settings.visuals.darkMode
@@ -30,10 +35,14 @@ export default {
       this.$forceUpdate()
     }
   },
+
   mounted () {
     if (this.updateFinished) {
       this.$forceUpdate()
     }
+
+    // hide the spinner
+    this.loading = false
   }
 }
 </script>
@@ -46,5 +55,19 @@ export default {
 
 footer.timer-footer {
   background-color: transparent;
+}
+
+.loading-enter-active {
+  @apply transition-all duration-1000;
+}
+
+.loading-enter {
+  @apply opacity-60;
+
+  clip-path: circle(0%);
+}
+
+.loading-enter-to {
+  clip-path: circle(100%);
 }
 </style>
