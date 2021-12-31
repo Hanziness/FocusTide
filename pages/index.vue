c355d827c2fd07bcbd60c169072907e0141d41a5
Imre Gera <hanziness@windowslive.com>
2021-12-31 15:27:15 +0100

    feat: Add compression to home page favicon

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,14 @@
     <transition name="fab-transition">
       <nuxt-link v-show="showFAB" v-slot="{ navigate }" to="/timer" custom>
         <div class="transition absolute w-full bottom-0 rounded-t-lg xl:w-auto xl:right-6 xl:bottom-4 z-10 xl:rounded-lg text-xl shadow-md px-4 py-3 bg-amber-300 hover:bg-amber-400 shadow-amber-300/30 hover:shadow-amber-300/60 active:duration-500 active:shadow-xl active:shadow-amber-300/80 active:bg-amber-500 text-black font-bold uppercase cursor-pointer flex flex-row space-x-2 items-center border border-neutral-100" @click="navigate">
-          <img src="/favicon.png" width="32" height="32" class="">
+          <nuxt-img
+            src="/favicon.png"
+            width="32"
+            height="32"
+            class=""
+            format="png"
+            quality="95"
+          />
           <span v-text="$i18n.t('index.launch')" />
         </div>
       </nuxt-link>
@@ -21,7 +28,7 @@
             <div class="flex flex-row items-start mr-16">
               <!-- App icon -->
               <div class="mr-4 mt-1 min-w-max min-h-max">
-                <img src="/favicon.png" width="68" height="68" class="bg-red-200 rounded-lg p-2">
+                <nuxt-img src="/favicon.png" width="68" height="68" class="bg-red-200 rounded-lg p-2" />
               </div>
               <!-- App name and slogan -->
               <div class="flex flex-col">
