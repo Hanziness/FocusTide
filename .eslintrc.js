661f63db4a4e6bec62d9b8404ca72479ba22c773
Imre Gera <hanziness@windowslive.com>
2022-01-02 11:40:32 +0100

    refactor(deps): Upgrade ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
   },
   extends: [
     '@nuxtjs',
