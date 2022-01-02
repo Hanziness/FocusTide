3255d8d5b5fb472a9e3ce838b63c6a5d905d8c83
Imre Gera <hanziness@windowslive.com>
2022-01-02 11:45:50 +0100

    refactor(deps): Update Stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  customSyntax: 'postcss-html',
   extends: [
-    'stylelint-config-standard'
+    'stylelint-config-standard',
+    'stylelint-config-recommended-vue'
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
