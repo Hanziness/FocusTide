5d9910b529e3295fefd34b0b03c87e5c9bd8d11a
Imre Gera <hanziness@windowslive.com>
2022-01-03 19:50:58 +0100

    fix(todo): Task items' labels behave much better They won't break on hover anymore and very long works will be broken so they don't overlap the controls.

--- components/todoList/item.vue
+++ components/todoList/item.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    :class="['bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 hover:shadow-sm rounded-md border-l-8 themed-border px-2 py-2 transition-all duration-200 flex flex-row items-center', { 'opacity-50 line-through italic': item.state === 2, 'cursor-move': showReorder, 'ring themed-ring': dragged || droptarget }]"
+    :class="['relative bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 hover:shadow-sm rounded-md border-l-8 themed-border px-2 py-2 transition-all duration-200 flex flex-row items-center', { 'opacity-50 line-through italic': item.state === 2, 'cursor-move': showReorder, 'ring themed-ring': dragged || droptarget }]"
     :style="{ '--theme': $store.state.settings.visuals[item.section].colour }"
     draggable
     @mouseenter="hovering = true"
@@ -10,22 +10,26 @@
     @dragend="dragged = false, $emit('dropfinish', item)"
     @dragenter="$emit('droptarget', item)"
   >
-    <div :class="['-ml-2 -my-2 mr-2 self-stretch themed-bg transition-all duration-75 text-white flex flex-row items-center', { 'w-0': !showReorder, 'w-6': showReorder }]">
+    <div :class="['absolute left-0 h-full -my-2 mr-2 self-stretch themed-bg transition-all duration-75 text-white flex flex-row items-center flex-shrink-0', showReorder ? 'w-6' : 'w-0']">
       <span v-show="showReorder">
         <IconMenu size="16" />
       </span>
     </div>
-    <div class="flex flex-col select-none">
-      <span>{{ item.title }}</span>
+    <div class="flex flex-col select-none mr-7 transition-all duration-75 min-w-0" :class="[showReorder ? 'translate-x-6' : 'translate-x-0']">
+      <span class="break-words">{{ item.title }}</span>
       <!-- <span class="text-sm">Description</span> -->
     </div>
+
     <span class="flex-grow" />
-    <transition name="slidein">
-      <button v-show="manage" class="transition-all duration-100" @click="$emit('delete')">
-        <IconDelete size="18" class="mr-1" />
-      </button>
-    </transition>
-    <span><input :checked="checked" type="checkbox" class="rounded w-5 h-5 mr-1 themed-checkbox dark:bg-gray-300" @input="checked = !checked"></span>
+
+    <div class="flex-shrink-0 flex flex-row items-center space-x-1">
+      <transition name="slidein">
+        <button v-show="manage" class="transition-all duration-100" @click="$emit('delete')">
+          <IconDelete size="18" class="mr-1" />
+        </button>
+      </transition>
+      <input :checked="checked" type="checkbox" class="rounded w-5 h-5 mr-1 themed-checkbox dark:bg-gray-300" @input="checked = !checked">
+    </div>
   </div>
 </template>
 
