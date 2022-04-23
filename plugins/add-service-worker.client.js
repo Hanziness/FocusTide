9688e54f5b157247698eaf3692ffeef29dd70a51
Imre Gera <hanziness@windowslive.com>
2022-04-23 22:27:11 +0200

    Revert "feat(pwa): Add custom service worker using NEW_SERVICE_WORKER env"
    This reverts commit 3c83651c26da94768101028c5389de6371168e9d.

--- plugins/add-service-worker.client.js
+++
@@ -1,7 +0,0 @@
-export default function () {
-  if (process.env.NEW_SERVICE_WORKER && !process.server && 'serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-    })
-  }
-}
