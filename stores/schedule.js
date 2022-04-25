bebb047700d3cb7afcb3baf9895b0e9d1fb3ba98
Imre Gera <hanziness@windowslive.com>
2022-04-25 18:26:54 +0200

    fix: Fix settings import in schedule store

--- stores/schedule.js
+++ stores/schedule.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import useSettings from './settings'
+import { useSettings } from './settings'
 
 export const useSchedule = defineStore('schedule', {
   state: () => ({
