b0da57fc1c54b9cf8a8fbf6ffb2cc7750fcc617c
Imre Gera <hanziness@windowslive.com>
2022-04-25 18:29:57 +0200

    refactor: Change store in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,17 @@
 <template>
-  <div :class="[{ 'dark': $store.state.settings.visuals.darkMode }]">
+  <div :class="[{ 'dark': visuals.darkMode }]">
     <nuxt />
   </div>
 </template>
 
 <script>
+import { mapState } from 'pinia'
+import { useSettings } from '@/stores/settings'
+
 export default {
-  name: 'LayoutDefault'
+  name: 'LayoutDefault',
+  computed: {
+    ...mapState(useSettings, ['visuals'])
+  }
 }
 </script>
