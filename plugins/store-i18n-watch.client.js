d1e17d374e2671b8fc0477e4449d279d3ad29bd3
Imre Gera <hanziness@windowslive.com>
2022-04-28 16:55:30 +0200

    feat: Implement locale watching

---
+++ plugins/store-i18n-watch.client.js
@@ -0,0 +1,22 @@
+const NuxtI18nWatcherPlugin = ({ app, $pinia }) => {
+  if (app.i18n) {
+    $pinia.use(({ store }) => {
+      // if settings.lang changes, update app locale
+      if (store.$id === 'settings') {
+        if (!store.$state.lang && app.i18n.locale) {
+          store.$state.lang = app.i18n.locale
+        }
+
+        store.$subscribe(() => {
+          if (store.$state.lang) {
+            app.i18n.setLocale(store.$state.lang)
+          }
+        })
+      }
+    })
+  } else {
+    console.warn('No i18n module found')
+  }
+}
+
+export default NuxtI18nWatcherPlugin
