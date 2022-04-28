0afa645ed9b490443f64af36ed0e208387f184df
Imre Gera <hanziness@windowslive.com>
2022-04-28 17:09:24 +0200

    feat: Add back reset functionality

--- plugins/store-persist.client.js
+++ plugins/store-persist.client.js
@@ -1,26 +1,44 @@
 const persistStores = ['settings', 'tasklist']
+const storeResetKey = '--reset-store'
 
 /** Get the persistence key of the store by its ID */
 const getStorePersistenceKey = id => `persist-${id}`
 
 /** Restore store to its persisted state (if there is such a state) */
 function restoreStore (store) {
-  const stateToRestore = localStorage.getItem(getStorePersistenceKey(store.$id))
+  const stateToRestore = JSON.parse(localStorage.getItem(getStorePersistenceKey(store.$id)))
 
   if (stateToRestore !== null) {
-    store.$patch(JSON.parse(stateToRestore))
+    store.$patch(stateToRestore)
   }
 }
 
 function PiniaPersistPlugin ({ store }) {
   if (persistStores.includes(store.$id)) {
+    const restore = localStorage.getItem(storeResetKey) == null
     // Restore the store first
-    restoreStore(store)
+    if (restore) {
+      restoreStore(store)
+    }
 
     // Subscribe to changes and persist them
     const unsubscribe = store.$subscribe(() => {
       try {
-        localStorage.setItem(getStorePersistenceKey(store.$id), JSON.stringify(store.$state))
+        if (store.$id === 'settings') {
+          if (store.$state.reset) {
+            // if the settings store is in reset state, set store reset key
+            localStorage.setItem(storeResetKey, '1')
+          } else {
+            // otherwise remove the reset key
+            localStorage.removeItem(storeResetKey)
+          }
+        }
+
+        // otherwise persist store
+        localStorage.setItem(
+          getStorePersistenceKey(store.$id),
+          JSON.stringify(store.$state)
+        )
       } catch (err) {
         // Failed to write to localStorage for some reason, unsubscribe persistence
         unsubscribe()
