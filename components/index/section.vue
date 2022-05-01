f779fd15fb685570dd506d4ed5a223450e044653
Imre Gera <hanziness@windowslive.com>
2022-05-01 13:53:00 +0200

    fix(home): Update alignment and remove snapping (#217)
    * refactor(home): Change section sizing
    
    * refactor(home): Remove snapping and improve spacing
    
    * refactor: Update screenshot on home page

--- components/index/section.vue
+++ components/index/section.vue
@@ -1,5 +1,5 @@
 <template functional>
-  <div :ref="data.ref" class="h-screen w-screen relative" :class="[data.class, data.staticClass]">
+  <div :ref="data.ref" class="relative w-screen" :class="[data.class, data.staticClass]">
     <slot name="before" />
     <slot />
     <slot name="after" />
