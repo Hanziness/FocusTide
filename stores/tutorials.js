a0ba9ccbbe047d4475b39b13e9348b2f5a669077
Imre Gera <hanziness@windowslive.com>
2022-05-16 19:24:01 +0200

    refactor: Remove default tutorials

--- stores/tutorials.js
+++ stores/tutorials.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useTutorials = defineStore('tutorials', {
   state: () => ({
-    openTutorials: ['pwa_installed']
+    openTutorials: []
   }),
 
   getters: {
