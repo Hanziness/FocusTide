df985e89e32830c1665627cfed16ef19704b420f
Imre Gera <hanziness@windowslive.com>
2022-05-26 20:06:43 +0200

    feat: Improve vertical layout of approximate timer

--- components/timer/display/timerApproximate.vue
+++ components/timer/display/timerApproximate.vue
@@ -1,15 +1,17 @@
 <template>
-  <div :class="['timer-display select-none flex flex-row gap-2 items-center leading-none text-9xl xl:text-[12rem]', { 'active': running }]">
-    <transition>
-      <div v-show="time.value < 0" class="-mr-4 font-bold">
-        +
-      </div>
-    </transition>
-    <transition name="transition-approximate-up" mode="out-in">
-      <div :key="time.value" :style="{ '--ch': Math.max(1, Math.ceil(Math.log10(Math.abs(time.value) + 1))) }" class="font-bold text-right time-value">
-        {{ Math.abs(time.value) }}
-      </div>
-    </transition>
+  <div :class="['timer-display select-none flex flex-col md:flex-row gap-2 items-center leading-none text-9xl xl:text-[12rem]', { 'active': running }]">
+    <div class="flex flex-row">
+      <transition name="transition-approximate-up">
+        <div v-show="time.value < 0" class="-mr-2 font-bold">
+          +
+        </div>
+      </transition>
+      <transition name="transition-approximate-up" mode="out-in">
+        <div :key="time.value" :style="{ '--ch': Math.max(1, Math.ceil(Math.log10(Math.abs(time.value) + 1))) }" class="font-bold text-center md:text-right time-value">
+          {{ Math.abs(time.value) }}
+        </div>
+      </transition>
+    </div>
     <transition name="transition-approximate-up" mode="out-in">
       <div :key="time.string" class="text-[.5em]">
         {{ time.string }}
