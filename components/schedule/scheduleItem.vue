d542cf667f1ac61352b3a2b1f2fced652027516f
Imre Gera <hanziness@windowslive.com>
2022-05-30 17:55:26 +0200

    fix: Fix schedule item transitions (#237)

--- components/schedule/scheduleItem.vue
+++ components/schedule/scheduleItem.vue
@@ -1,9 +1,9 @@
 <template>
   <div
+    class="w-12 h-12 border rounded-lg"
     :class="[
-      'mr-2 inline-block w-12 h-12 relative border border-transparent rounded-lg p-0 last-of-type:mr-0',
       { 'border-gray-100 shadow': active },
-      { 'shadow-sm opacity-40': !active }
+      { 'shadow-sm opacity-40 border-transparent': !active }
     ]"
     :style="[
       { 'background-color': visuals[data.type].colour }
