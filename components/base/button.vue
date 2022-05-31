9ea03448479721ac8fdf4299629a25ec1d86506a
Imre Gera <hanziness@windowslive.com>
2022-05-31 18:03:50 +0200

    refactor: Change button component to be more flexible

--- components/base/button.vue
+++ components/base/button.vue
@@ -1,15 +1,14 @@
 <template functional>
-  <Component
-    :is="props.tag"
+  <button
     :ref="data.ref"
+    class="relative p-2 overflow-hidden transition duration-300 rounded-full before:transition before:absolute before:opacity-0 before:bg-black before:w-full before:h-full before:left-0 before:top-0 before:mix-blend-overlay"
     :class="[
-      'transition-colors',
-      {
-        'bg-transparent rounded-full p-2 hover:bg-gray-200 dark:hover:bg-gray-50 dark:hover:bg-opacity-20 active:bg-gray-400 dark:active:bg-gray-50 dark:active:bg-opacity-40': !props.noStyle && props.subtle,
-        'bg-gray-400 hover:bg-gray-500 active:bg-gray-600 rounded p-4': !props.noStyle && !props.subtle,
-        'cursor-pointer': !props.disabled,
-        'opacity-50': props.disabled
-      },
+      { 'px-3': !props.circle },
+      { 'opacity-60 pointer-events-none': props.disabled },
+      { 'bg-theme border-theme ring-theme shadow-theme': props.default },
+      { 'bg-opacity-100 hover:shadow-md hover:before:opacity-10 active:before:opacity-30': props.importance === 1 },
+      { 'border-2 bg-opacity-0 hover:bg-theme active:before:opacity-20 active:shadow-sm': props.importance === 2 },
+      { 'bg-opacity-0 hover:bg-opacity-30 active:bg-opacity-60 focus:ring': props.importance === 3 },
       data.staticClass,
       data.class
     ]"
@@ -19,29 +18,40 @@
     :disabled="props.disabled"
     v-on="listeners"
   >
-    <slot />
-  </Component>
+    <slot>
+      {{ (data.domProps && data.domProps.textContent) ? data.domProps.textContent : null }}
+    </slot>
+  </button>
 </template>
 
 <script>
 export default {
   name: 'ControlButton',
   props: {
-    tag: {
-      default: 'button',
-      type: String
-    },
     disabled: {
       default: false,
       type: Boolean
     },
-    subtle: {
+
+    /** Apply default colour scheme to the button */
+    default: {
       default: false,
       type: Boolean
     },
-    noStyle: {
+
+    /** Make padding equal on all sides for circular buttons */
+    circle: {
       default: false,
       type: Boolean
+    },
+
+    /** The higher the number, the less important the button is. 1 is for primary buttons,
+     * 2 for secondary buttons and 3 for tertiary buttons.
+     */
+    importance: {
+      type: Number,
+      default: 1,
+      validator: value => value >= 1 && value <= 3
     }
   }
 }
