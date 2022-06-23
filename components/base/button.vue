f1f48f7787ad0586d596006c1d4a00bd05bccc58
Imre Gera <hanziness@windowslive.com>
2022-06-23 15:54:28 +0200

    feat(button): Improve button click responses

--- components/base/button.vue
+++ components/base/button.vue
@@ -5,10 +5,11 @@
     class="relative p-3 overflow-hidden transition duration-300 rounded-full before:transition before:absolute before:opacity-0 before:w-full before:h-full before:left-0 before:top-0"
     :class="[
       { 'px-4': !props.circle },
-      { 'opacity-60 pointer-events-none': props.disabled },
+      { 'opacity-40 pointer-events-none': props.disabled },
       { 'bg-theme border-theme ring-theme shadow-theme': props.default },
+      { 'hover:before:opacity-10 active:before:opacity-30 active:scale-110': props.extendedBaseStyles },
       { 'text-center': props.link },
-      { 'before:bg-black text-black before:mix-blend-overlay': !props.dark, 'before:bg-slate-50 text-slate-50': props.dark },
+      { 'before:bg-black': !props.dark, 'before:bg-slate-50': props.dark },
       { 'bg-opacity-100 hover:shadow-sm hover:before:opacity-20 active:before:opacity-30 active:shadow-lg': props.importance === 1 },
       { 'border-2 bg-opacity-0 hover:bg-theme active:before:opacity-20 active:shadow-sm': props.importance === 2 },
       { 'bg-opacity-0 hover:bg-opacity-30 active:bg-opacity-60 focus:ring': props.importance === 3 },
@@ -45,6 +46,12 @@ export default {
     /** Apply default colour scheme to the button */
     default: {
       default: false,
+      type: Boolean
+    },
+
+    /** Adds some more classes, like scaling on active and before:background opacity for hover and active */
+    extendedBaseStyles: {
+      default: true,
       type: Boolean
     },
 
