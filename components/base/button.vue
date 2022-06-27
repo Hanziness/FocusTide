6d9bdebbfdfd2e4329982ce01eba3be38ac38116
Imre Gera <hanziness@windowslive.com>
2022-06-27 21:09:49 +0200

    refactor(button): Make only the background scale on interaction

--- components/base/button.vue
+++ components/base/button.vue
@@ -2,17 +2,10 @@
   <Component
     :is="props.link ? 'a' : 'button'"
     :ref="data.ref"
-    class="relative p-3 overflow-hidden transition duration-300 rounded-full before:transition before:absolute before:opacity-0 before:w-full before:h-full before:left-0 before:top-0"
+    class="group"
     :class="[
-      { 'px-4': !props.circle },
       { 'opacity-40 pointer-events-none': props.disabled },
-      { 'bg-theme border-theme ring-theme shadow-theme': props.default },
-      { 'hover:before:opacity-10 active:before:opacity-30 active:scale-110': props.extendedBaseStyles },
       { 'text-center': props.link },
-      { 'before:bg-black': !props.dark, 'before:bg-slate-50': props.dark },
-      { 'bg-opacity-100 hover:shadow-sm hover:before:opacity-20 active:before:opacity-30 active:shadow-lg': props.importance === 1 },
-      { 'border-2 bg-opacity-0 hover:bg-theme active:before:opacity-20 active:shadow-sm': props.importance === 2 },
-      { 'bg-opacity-0 hover:bg-opacity-30 active:bg-opacity-60 focus:ring': props.importance === 3 },
       data.staticClass,
       data.class
     ]"
@@ -22,10 +15,33 @@
     :disabled="props.disabled"
     v-on="listeners"
   >
+    <!-- Backdrop -->
     <div class="relative">
-      <slot>
-        {{ (data.domProps && data.domProps.textContent) ? data.domProps.textContent : null }}
-      </slot>
+      <div
+        class="absolute w-full h-full overflow-hidden transition duration-300 rounded-full before:transition before:absolute before:opacity-0 before:w-full before:h-full before:left-0 before:top-0"
+        :class="[
+          { 'bg-theme border-theme ring-theme shadow-theme': props.default },
+          { 'group-hover:before:opacity-10 group-active:before:opacity-30': props.extendedBaseStyles },
+          { 'group-active:scale-105 group-active:scale-y-110': props.extendedBaseStyles && !props.circle },
+          { 'group-active:scale-110': props.extendedBaseStyles && props.circle },
+          { 'before:bg-black dark:before:bg-slate-50': !props.dark, 'before:bg-slate-50': props.dark },
+          { 'bg-opacity-100 group-hover:shadow-sm group-hover:before:opacity-20 group-active:before:opacity-25 group-active:shadow-lg': props.importance === 1 },
+          { 'border-2 bg-opacity-0 hover:bg-theme group-active:before:opacity-20 group-active:shadow-sm': props.importance === 2 },
+          { 'bg-opacity-0 group-hover:bg-opacity-30 group-active:bg-opacity-60 group-focus:ring': props.importance === 3 }
+        ]"
+      />
+
+      <!-- Contents -->
+      <div
+        class="relative p-3"
+        :class="[
+          { 'px-4': !props.circle },
+        ]"
+      >
+        <slot>
+          {{ (data.domProps && data.domProps.textContent) ? data.domProps.textContent : null }}
+        </slot>
+      </div>
     </div>
   </Component>
 </template>
