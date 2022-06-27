8ef9b0b0142d40e66023c4d95e4eec5f5cf1c468
Imre Gera <hanziness@windowslive.com>
2022-06-27 21:24:19 +0200

    refactor(button): Revise backdrop tintint behaviour

--- components/base/button.vue
+++ components/base/button.vue
@@ -1,4 +1,10 @@
 <template functional>
+  <!--
+    Element styling setup:
+    - ::before - darkening/lightening on the backdrop (used for hover and active states, effect strength is driven by the opacity of the ::before pseudo-element)
+    - bg       - coloured background of the button
+  -->
+
   <Component
     :is="props.link ? 'a' : 'button'"
     :ref="data.ref"
@@ -21,13 +27,13 @@
         class="absolute w-full h-full overflow-hidden transition duration-300 rounded-full before:transition before:absolute before:opacity-0 before:w-full before:h-full before:left-0 before:top-0"
         :class="[
           { 'bg-theme border-theme ring-theme shadow-theme': props.default },
-          { 'group-hover:before:opacity-10 group-active:before:opacity-30': props.extendedBaseStyles },
+          { '': props.extendedBaseStyles },
           { 'group-active:scale-105 group-active:scale-y-110': props.extendedBaseStyles && !props.circle },
           { 'group-active:scale-110': props.extendedBaseStyles && props.circle },
           { 'before:bg-black dark:before:bg-slate-50': !props.dark, 'before:bg-slate-50': props.dark },
-          { 'bg-opacity-100 group-hover:shadow-sm group-hover:before:opacity-20 group-active:before:opacity-25 group-active:shadow-lg': props.importance === 1 },
-          { 'border-2 bg-opacity-0 hover:bg-theme group-active:before:opacity-20 group-active:shadow-sm': props.importance === 2 },
-          { 'bg-opacity-0 group-hover:bg-opacity-30 group-active:bg-opacity-60 group-focus:ring': props.importance === 3 }
+          { 'bg-opacity-100 group-hover:shadow-sm group-hover:before:opacity-10 group-active:before:opacity-25 group-active:shadow-lg': props.importance === 1 },
+          { 'border-2 bg-opacity-0 group-hover:bg-opacity-30 group-active:bg-opacity-40 group-active:shadow-sm group-hover:before:opacity-10 group-active:before:opacity-10': props.importance === 2 },
+          { 'bg-opacity-0 group-hover:bg-opacity-30 group-active:bg-opacity-40 group-focus:ring': props.importance === 3 }
         ]"
       />
 
