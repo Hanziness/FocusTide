73ff2cc1c6cab21a5b1149f530a1a2118af3ec8a
Imre Gera <hanziness@windowslive.com>
2022-07-21 22:30:51 +0200

    refactor: Remove debug statement from i18nlanguages

--- plugins/i18nlanguages.js
+++ plugins/i18nlanguages.js
@@ -4,8 +4,6 @@ import { defineNuxtPlugin } from '#app'
 export default defineNuxtPlugin((nuxtApp) => {
   const availableLocales = {}
 
-  console.log(nuxtApp.vueApp.i18n.locales._value)
-
   if (nuxtApp.vueApp.i18n) {
     for (const lang of nuxtApp.vueApp.i18n.locales.value) {
       availableLocales[lang.code] = lang.name
