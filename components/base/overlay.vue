c82cee9d00a1a1078a7518b704606f34499d8957
Imre Gera <hanziness@windowslive.com>
2022-08-06 21:18:20 +0200

    refactor: Make overlay adaptable for backdrop filters

--- components/base/overlay.vue
+++ components/base/overlay.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed z-20 w-full h-full overflow-hidden transition-colors duration-300 bg-black bg-opacity-60">
+  <div class="fixed z-20 w-full h-full overflow-hidden transition-colors duration-300 before:bg-black before:bg-opacity-60 before:absolute before:left-0 before:w-full before:h-full before:transition-all">
     <slot class="z-30" />
   </div>
 </template>
