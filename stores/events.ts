92972657b004c602ff0e03bc2b40789db5aa0148
Imre Gera <hanziness@windowslive.com>
2022-08-07 22:18:04 +0200

    refactor(events): Make events store more type-friendly

--- stores/events.ts
+++ stores/events.ts
@@ -18,7 +18,27 @@ export const state = () => ({
   eventList: []
 })
 
+export enum EventType {
+  FOCUS_GAIN = 'focus.gain',
+  FOCUS_LOST = 'focus.lost',
+  TIMER_START = 'timer.start',
+  TIMER_PAUSE = 'timer.pause',
+  TIMER_STOP = 'timer.stop',
+  TIMER_FINISH = 'timer.complete',
+  SCHEDULE_ADVANCE_MANUAL = 'schedule.adv.manual',
+  SCHEDULE_ADVANCE_AUTO = 'schedule.adv.auto',
+  APP_STARTED = 'app.start',
+  APP_ERROR = 'app.error',
+  OTHER = 'other'
+}
+
 export class Event {
+  _timestamp: Date
+  _event: EventType
+
+  /** Any additional payload for the event can be added here */
+  _data?: unknown
+
   constructor (eventType = EventType.OTHER, { data = undefined, timestamp = new Date() }) {
     this._timestamp = timestamp
     this._event = eventType
@@ -27,18 +47,4 @@ export class Event {
       this._data = data
     }
   }
-}
-
-export const EventType = {
-  FOCUS_GAIN: 'focus.gain',
-  FOCUS_LOST: 'focus.lost',
-  TIMER_START: 'timer.start',
-  TIMER_PAUSE: 'timer.pause',
-  TIMER_STOP: 'timer.stop',
-  TIMER_FINISH: 'timer.complete',
-  SCHEDULE_ADVANCE_MANUAL: 'schedule.adv.manual',
-  SCHEDULE_ADVANCE_AUTO: 'schedule.adv.auto',
-  APP_STARTED: 'app.start',
-  APP_ERROR: 'app.error',
-  OTHER: 'other'
 }
