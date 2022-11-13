bb859a257bbfc5439905622073570d7a02c3b222
Imre Gera <hanziness@windowslive.com>
2022-11-13 23:17:14 +0100

    refactor(appbar): Shrink section type instead of display

--- components/appBar.vue
+++ components/appBar.vue
@@ -13,12 +13,12 @@ const settingsStore = useSettings()
 
 <template>
   <div class="flex flex-row items-center w-full gap-2 px-4 my-1 isolate h-14" :style="{ '--theme': settingsStore.visuals.darkMode ? '245 245 244' : '15 23 42' }">
-    <div class="h-10 px-2 py-2 rounded-full bg-themed">
+    <div class="flex-shrink-0 h-10 px-2 py-2 rounded-full bg-themed">
       <ClientOnly>
         <ScheduleView />
       </ClientOnly>
     </div>
-    <div class="text-lg select-none" v-text="$t('section.' + scheduleStore.getCurrentItem.type).toLowerCase()" />
+    <div class="flex-shrink overflow-hidden text-lg select-none text-ellipsis" v-text="$t('section.' + scheduleStore.getCurrentItem.type).toLowerCase()" />
     <div class="flex-grow" />
     <CButton
       v-if="settingsStore.tasks.enabled"
