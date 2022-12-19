c13adfb5d1dfaae18ba1c09722d9bb06caeb6876
Imre Gera <hanziness@windowslive.com>
2022-12-19 15:32:24 +0100

    refactor(button): Improve importance theming

--- components/base/uiButton.vue
+++ components/base/uiButton.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { PropType } from 'vue'
-import { ButtonTheme, MotionTheme } from './types/button'
+import { ButtonImportance, ButtonTheme, MotionTheme } from './types/button'
 
 const props = defineProps({
   disabled: {
@@ -51,7 +51,7 @@ const props = defineProps({
   },
 
   /** Apply default colour scheme to the button */
-  defaultStyle: {
+  noDefaultStyle: {
     default: false,
     type: Boolean
   },
@@ -68,13 +68,10 @@ const props = defineProps({
     type: Boolean
   },
 
-  /** The higher the number, the less important the button is. 1 is for primary buttons,
-   * 2 for secondary buttons and 3 for tertiary buttons.
-   */
+  /** Defines how important a button appears to be. */
   importance: {
-    type: Number,
-    default: 1,
-    validator: (value: number) => value >= 1 && value <= 3
+    type: String as PropType<ButtonImportance>,
+    default: ButtonImportance.Filled
   }
 })
 </script>
@@ -86,52 +83,47 @@ const props = defineProps({
     - bg       - coloured background of the button
   -->
 
-  <Component
-    :is="link ? 'a' : 'button'"
-    class="group"
-    :class="[
-      { 'opacity-40 pointer-events-none': props.disabled },
-      { 'text-center': props.link },
-      { 'aspect-square': props.circle}
-    ]"
-    :aria-disabled="props.disabled"
-    :disabled="props.disabled"
-    :type="props.link ? undefined : 'button'"
-  >
+  <Component :is="link ? 'a' : 'button'" class="group" :class="[
+    { 'opacity-40 pointer-events-none': props.disabled },
+    { 'text-center': props.link },
+    { 'aspect-square': props.circle }
+  ]" :aria-disabled="props.disabled" :disabled="props.disabled" :type="props.link ? undefined : 'button'">
     <!-- Backdrop -->
     <div class="relative w-full h-full isolate">
       <div
         class="absolute w-full h-full overflow-hidden transition duration-300 rounded-full -z-10 before:transition before:absolute before:opacity-0 before:w-full before:h-full before:left-0 before:top-0"
         :class="[
-          { 'bg-primary-container border-primary-container ring-primary-container shadow-primary dark:bg-primary-darkcontainer dark:border-primary-darkcontainer dark:ring-primary-darkcontainer': props.theme === ButtonTheme.Primary},
-          { 'bg-secondary-container border-secondary-container ring-secondary-container shadow-secondary dark:bg-secondary-darkcontainer dark:border-secondary-darkcontainer dark:ring-secondary-darkcontainer': props.theme === ButtonTheme.Secondary },
-          { 'bg-surface-light border-surface-light ring-surface-light dark:bg-surface-dark dark:border-surface-dark dark:ring-surface-dark': props.theme === ButtonTheme.Neutral },
-          {'bg-surface-light border-surface-light ring-surface-light': props.theme === ButtonTheme.NeutralWhite},
-          { 'bg-surface-dark border-surface-dark ring-surface-dark': props.theme === ButtonTheme.NeutralDark},
+          { 'bg-primary border-primary ring-primary dark:bg-primary-dark dark:border-primary-dark dark:ring-primary-dark': !props.noDefaultStyle && props.theme === ButtonTheme.Primary && props.importance === ButtonImportance.Filled },
+          { 'bg-primary-container border-primary-container ring-primary-container dark:bg-primary-darkcontainer dark:border-primary-darkcontainer dark:ring-primary-darkcontainer': !props.noDefaultStyle && props.theme === ButtonTheme.Primary && props.importance !== ButtonImportance.Filled },
+          { 'bg-secondary border-secondary ring-secondary dark:bg-secondary-dark dark:border-secondary-dark dark:ring-secondary-dark': !props.noDefaultStyle && props.theme === ButtonTheme.Secondary && props.importance === ButtonImportance.Filled },
+          { 'bg-secondary-container border-secondary-container ring-secondary-container shadow-secondary dark:bg-secondary-darkcontainer dark:border-secondary-darkcontainer dark:ring-secondary-darkcontainer': !props.noDefaultStyle && props.theme === ButtonTheme.Secondary && props.importance !== ButtonImportance.Filled },
+          { 'bg-surface-light border-surface-light ring-surface-light dark:bg-surface-dark dark:border-surface-dark dark:ring-surface-dark': !props.noDefaultStyle && props.theme === ButtonTheme.Neutral },
+          { 'bg-surface-light border-surface-light ring-surface-light': !props.noDefaultStyle && props.theme === ButtonTheme.NeutralWhite },
+          { 'bg-surface-dark border-surface-dark ring-surface-dark': !props.noDefaultStyle && props.theme === ButtonTheme.NeutralDark },
           { 'group-active:scale-x-110': props.motion === MotionTheme.Horizontal || props.motion === MotionTheme.All },
           { 'group-active:scale-y-110': props.motion === MotionTheme.All },
           { 'before:bg-surface-dark dark:before:bg-surface-light': !props.dark, 'before:bg-surface-light': props.dark },
-          { 'bg-opacity-100 group-hover:shadow-sm group-hover:before:opacity-10 group-active:before:opacity-25 group-active:shadow-lg ring-0 border-0': importance === 1 },
-          { 'border-2 bg-opacity-0 dark:bg-opacity-0 group-hover:bg-opacity-100 group-active:bg-opacity-100 group-active:shadow-sm group-active:before:opacity-10': importance === 2 },
-          { 'bg-opacity-0 dark:bg-opacity-0 group-hover:bg-opacity-100 group-active:bg-opacity-100 group-focus:ring': importance === 3 },
+          { 'bg-opacity-100 group-hover:shadow-sm group-hover:before:opacity-10 group-active:before:opacity-25 group-active:shadow-lg ring-0 border-0': [ButtonImportance.Filled, ButtonImportance.Tonal].includes(props.importance) },
+          { 'bg-opacity-0 dark:bg-opacity-0 group-hover:bg-opacity-100 group-active:bg-opacity-100 group-focus:ring': [ButtonImportance.Outline, ButtonImportance.Text].includes(props.importance) },
+          { 'border-2': props.importance === ButtonImportance.Outline },
           props.bgClass
-        ]"
-      />
+        ]" />
 
       <!-- Contents -->
-      <div
-        class="grid items-center justify-center w-full h-full grid-flow-col"
-        :class="[
-          { 'p-3': !props.noPadding },
-          { 'px-4': !props.circle && !props.noPadding },
-          { 'text-primary-oncontainer dark:text-primary-darkoncontainer': !props.noContentTheme && props.theme === ButtonTheme.Primary },
-          { 'text-secondary-oncontainer dark:text-secondary-darkoncontainer': !props.noContentTheme && props.theme === ButtonTheme.Secondary },
-          { 'text-surface-onlight': !props.noContentTheme && (props.theme === ButtonTheme.NeutralWhite || props.theme === ButtonTheme.Neutral) },
-          { 'dark:text-surface-ondark': !props.noContentTheme && props.theme === ButtonTheme.Neutral },
-          { 'text-surface-ondark': !props.noContentTheme && props.theme === ButtonTheme.NeutralDark},
-          props.innerClass,
-        ]"
-      >
+      <div class="grid items-center justify-center w-full h-full grid-flow-col" :class="[
+        { 'p-3': !props.noPadding },
+        { 'px-4': !props.circle && !props.noPadding },
+        { 'text-white dark:text-primary-darkon': !props.noContentTheme && props.theme === ButtonTheme.Primary && props.importance === ButtonImportance.Filled },
+        { 'text-primary-oncontainer dark:text-primary-darkoncontainer': !props.noContentTheme && props.theme === ButtonTheme.Primary && props.importance === ButtonImportance.Outline },
+        { 'text-primary dark:text-primary-dark': !props.noContentTheme && props.theme === ButtonTheme.Primary && [ButtonImportance.Outline, ButtonImportance.Text].includes(props.importance) },
+        { 'text-white dark:text-secondary-darkon': !props.noContentTheme && props.theme === ButtonTheme.Secondary && props.importance === ButtonImportance.Filled },
+        { 'text-secondary-oncontainer dark:text-secondary-darkoncontainer': !props.noContentTheme && props.theme === ButtonTheme.Secondary && props.importance === ButtonImportance.Outline },
+        { 'text-secondary dark:text-secondary-dark': !props.noContentTheme && props.theme === ButtonTheme.Secondary && [ButtonImportance.Outline, ButtonImportance.Text].includes(props.importance) },
+        { 'text-surface-onlight': !props.noContentTheme && (props.theme === ButtonTheme.NeutralWhite || props.theme === ButtonTheme.Neutral) },
+        { 'dark:text-surface-ondark': !props.noContentTheme && props.theme === ButtonTheme.Neutral },
+        { 'text-surface-ondark': !props.noContentTheme && props.theme === ButtonTheme.NeutralDark },
+        props.innerClass,
+      ]">
         <slot />
       </div>
     </div>
