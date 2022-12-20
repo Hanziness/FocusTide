472618766175cd0e537d4f619738d616d31e84c9
Imre Gera <hanziness@windowslive.com>
2022-12-20 19:21:14 +0100

    feat: Add onboarding page and header components

---
+++ components/tutorial/onboarding/onboardingHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="px-6 py-3 flex gap-2 text-lg flex-row justify-center items-center">
+    <slot />
+    <span v-text="props.text" />
+  </div>
+</template>
