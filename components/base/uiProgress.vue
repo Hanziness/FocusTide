02caa14dee1225328425b0cd5fdc916579cdcbf4
Imre Gera <hanziness@windowslive.com>
2022-12-20 19:20:59 +0100

    refactor(progress): Update progress bar colours

--- components/base/uiProgress.vue
+++ components/base/uiProgress.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="rounded-full h-2 bg-primary-container dark:bg-primary-darkcontainer overflow-hidden">
-    <div class="rounded-full bg-primary dark:bg-primary-dark w-full h-full transition-all" :style="{ 'translate': `-${100 - 100 * props.progress}% 0` }"></div>
+  <div class="rounded-full h-2 bg-surface-variant dark:bg-surface-darkvariant overflow-hidden ring-1 ring-inset ring-surface-onlight ring-opacity-10 dark:ring-surface-ondark dark:ring-opacity-25">
+    <div class="rounded-full bg-primary dark:bg-primary-dark w-full h-full transition-all" :style="{ 'translate': `-${100 - 100 * props.progress}% 0` }" />
   </div>
 </template>
