c8a56dc07a7684b70e70c425b80f5c99366a1430
Imre Gera <hanziness@windowslive.com>
2022-12-20 19:38:35 +0100

    feat(onboarding): Add setup steps to onboarding (#291)
    * feat: Add progress bar
    
    * feat(onboarding): Start adding new onboarding pages with settings
    
    * refactor(progress): Update progress bar colours
    
    * feat: Add onboarding page and header components
    
    * feat: Implement onboarding with settings pages
    
    * chore(i18n): Update Hungarian translations

---
+++ components/base/uiProgress.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  progress: {
+    type: Number,
+    default: 1.0,
+    validator: (value: number) => value >= 0.0 && value <= 1.0
+  }
+})
+
+</script>
+
+<template>
+  <div class="rounded-full h-2 bg-surface-variant dark:bg-surface-darkvariant overflow-hidden ring-1 ring-inset ring-surface-onlight ring-opacity-10 dark:ring-surface-ondark dark:ring-opacity-25">
+    <div class="rounded-full bg-primary dark:bg-primary-dark w-full h-full transition-all" :style="{ 'translate': `-${100 - 100 * props.progress}% 0` }" />
+  </div>
+</template>
---
+++ components/tutorial/onboarding/page2_timers.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import OptionGroup from '~~/components/base/optionGroup.vue'
+</script>
+
+<template>
+  <div class="h-96 flex flex-col">
+    <OptionGroup :choices="{'pomodoro': 'Pomodoro', 'short': 'Short times'}" :value="'short'" />
+  </div>
+</template>
