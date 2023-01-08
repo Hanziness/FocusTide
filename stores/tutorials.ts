1dd11b99add48bca3c55a8b6e06942cd470da059
Imre Gera <hanziness@windowslive.com>
2023-01-08 10:53:50 +0100

    fix(tutorials): Improve tutorials store types

--- stores/tutorials.ts
+++ stores/tutorials.ts
@@ -2,25 +2,25 @@ import { defineStore } from 'pinia'
 
 export const useTutorials = defineStore('tutorials', {
   state: () => ({
-    openTutorials: []
+    openTutorials: [] as string[]
   }),
 
   getters: {
     isTutorialOpen: (state) => {
-      return tutorialId => state.openTutorials.indexOf(tutorialId) === 0
+      return (tutorialId: string) => state.openTutorials.indexOf(tutorialId) === 0
     },
 
     currentTutorial: state => state.openTutorials[0] ?? null
   },
 
   actions: {
-    openTutorial (tutorialId) {
+    openTutorial (tutorialId: string) {
       if (!this.openTutorials.includes(tutorialId)) {
         this.openTutorials.push(tutorialId)
       }
     },
 
-    closeTutorial (tutorialId) {
+    closeTutorial (tutorialId: string) {
       const tutorialIndex = this.openTutorials.indexOf(tutorialId)
       if (tutorialIndex >= 0) {
         this.openTutorials.splice(tutorialIndex, 1)
