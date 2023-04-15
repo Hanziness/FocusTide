c5464298a08cb3e3de5149ff95ac371cd4931cea
Imre Gera <hanziness@windowslive.com>
2023-04-15 21:19:19 +0200

    feat: Add pop-up about coming from old domain

--- components/tutorial/_tutorialView.vue
+++ components/tutorial/_tutorialView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { markRaw } from 'vue'
 import tutorialOnboarding from './tutorialOnboarding.vue'
+import tutorialOldDomain from './tutorialOldDomain.vue'
 import { useTutorials } from '~~/stores/tutorials'
 import { useMain } from '~~/stores/main'
 
@@ -8,7 +9,8 @@ const tutorialsStore = useTutorials()
 const mainStore = useMain()
 
 const tutorials = {
-  onboarding: markRaw(tutorialOnboarding)
+  onboarding: markRaw(tutorialOnboarding),
+  oldDomain: markRaw(tutorialOldDomain)
 }
 
 const state = reactive({
@@ -24,10 +26,15 @@ watch(() => tutorialsStore.currentTutorial, (newValue) => {
 })
 
 onMounted(() => {
+  const route = useRoute()
   state.enableComponent = tutorialsStore.currentTutorial != null
 
   if (!mainStore.restoredStores.includes('tutorials')) {
     tutorialsStore.openTutorial('onboarding')
+  }
+
+  if (route.query.olddomain !== undefined) {
+    tutorialsStore.openTutorial('oldDomain')
   }
 })
 </script>
