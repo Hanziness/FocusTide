61820174e2afb5003130d627ffcfe2f78d963bb8
Imre Gera <hanziness@windowslive.com>
2024-09-20 15:18:44 +0000

    fix(pwa): Switch 'code' cache to StaleWhileRevalidate

--- modules/build/pwa.ts
+++ modules/build/pwa.ts
@@ -1,6 +1,6 @@
-import { defineNuxtModule } from '@nuxt/kit'
 import { join } from 'node:path'
 import { readdirSync, rmSync, existsSync } from 'fs'
+import { defineNuxtModule } from '@nuxt/kit'
 import { generateSW } from 'workbox-build'
 
 interface PWAModuleOptions {
@@ -10,77 +10,77 @@ interface PWAModuleOptions {
 const outputPath = 'serviceworker'
 
 export default defineNuxtModule({
-    setup(moduleOptions: PWAModuleOptions, nuxt) {
-        nuxt.hook('nitro:config', async (nitroConfig) => {
-            console.log(moduleOptions)
+  setup (moduleOptions: PWAModuleOptions, nuxt) {
+    nuxt.hook('nitro:config', async (nitroConfig) => {
+      console.log(moduleOptions)
 
-            const outputDir = join('.build', outputPath)
+      const outputDir = join('.build', outputPath)
 
-            if (existsSync(outputDir)) {
-                readdirSync(outputDir).filter(file => file.startsWith('workbox-')).forEach((file) => {
-                    console.info(`Removing ${join(outputDir, file)}`)
-                    rmSync(join(outputDir, file))
-                })
-            }
-
-            await generateSW({
-                swDest: join(outputDir, moduleOptions.swPath),
-                globDirectory: outputDir,
-                globPatterns: [
-                '**/*.{js,json,mjs,css,html,svg,woff2}'
-                ],
-                sourcemap: false,
-                navigationPreload: true,
-                runtimeCaching: [
-                {
-                    urlPattern: ({ url }) => /\.(js|json|css)$/.exec(url.pathname) !== null,
-                    handler: 'CacheFirst',
-                    options: {
-                    cacheName: 'code',
-                        cacheableResponse: {
-                            statuses: [0, 200]
-                        }
-                    }
-                },
-                {
-                    urlPattern: ({ url }) => /\.(jpg|png|jpeg|svg)$/.exec(url.pathname) !== null,
-                    handler: 'CacheFirst',
-                    options: {
-                    cacheName: 'assets',
-                        cacheableResponse: {
-                            statuses: [200]
-                        }
-                    }
-                },
-                {
-                    urlPattern: ({ url }) => /\.(woff2)$/.exec(url.pathname) !== null,
-                    handler: 'StaleWhileRevalidate',
-                    options: {
-                    cacheName: 'fonts',
-                        cacheableResponse: {
-                            statuses: [0, 200]
-                        }
-                    }
-                },
-                {
-                    urlPattern: ({ url }) => ['', '/', '/index', '/index.html'].includes(url.pathname),
-                    handler: 'NetworkFirst',
-                    options: {
-                        cacheableResponse: {
-                            statuses: [0, 200]
-                        }
-                    }
-                }
-                ]
-            })
-            console.info(`Generated service worker at ${join(outputDir, moduleOptions.swPath)}`)
-
-            nitroConfig.publicAssets ||= []
-            nitroConfig.publicAssets.push({
-                baseURL: '/',
-                dir: join(nuxt.options.rootDir, '.build', outputPath),
-                maxAge: 60 * 60 * 24 * 365 // 1 year
-            })
+      if (existsSync(outputDir)) {
+        readdirSync(outputDir).filter(file => file.startsWith('workbox-')).forEach((file) => {
+          console.info(`Removing ${join(outputDir, file)}`)
+          rmSync(join(outputDir, file))
         })
-    },
+      }
+
+      await generateSW({
+        swDest: join(outputDir, moduleOptions.swPath),
+        globDirectory: outputDir,
+        globPatterns: [
+          '**/*.{js,json,mjs,css,html,svg,woff2}'
+        ],
+        sourcemap: false,
+        navigationPreload: true,
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => /\.(js|json|css)$/.exec(url.pathname) !== null,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'code',
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            urlPattern: ({ url }) => /\.(jpg|png|jpeg|svg)$/.exec(url.pathname) !== null,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'assets',
+              cacheableResponse: {
+                statuses: [200]
+              }
+            }
+          },
+          {
+            urlPattern: ({ url }) => /\.(woff2)$/.exec(url.pathname) !== null,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'fonts',
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            urlPattern: ({ url }) => ['', '/', '/index', '/index.html'].includes(url.pathname),
+            handler: 'NetworkFirst',
+            options: {
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      })
+      console.info(`Generated service worker at ${join(outputDir, moduleOptions.swPath)}`)
+
+      nitroConfig.publicAssets ||= []
+      nitroConfig.publicAssets.push({
+        baseURL: '/',
+        dir: join(nuxt.options.rootDir, '.build', outputPath),
+        maxAge: 60 * 60 * 24 * 365 // 1 year
+      })
+    })
+  }
 })
